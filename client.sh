8c873b1144a26e0a63bab7f6fd389138274df8a4
lukicsl <lukic@gmx.ch>
2019-01-09 18:02:27 +0100

    Update client.sh

--- client.sh
+++ client.sh
@@ -2,6 +2,9 @@
 echo $LC_SRV:$GVOL $LC_DIR
 echo $$
 trap 'trap - TERM; umount $LC_DIR; kill -s TERM -- -$$' TERM
+if (cat /etc/hostname-host) then
+  LC_SRV=`cat /etc/hostname-host`
+fi
 while true; do
   mount.glusterfs $LC_SRV:$GVOL $LC_DIR
   if [ $? -eq 0 ]
