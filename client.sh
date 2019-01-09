5dc42f308edbf19b497752c1f22b4f801d92dff2
lukicsl <lukic@gmx.ch>
2019-01-09 14:51:48 +0100

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo $$
-trap 'trap - TERM; umount /datavol; kill -s TERM -- -$$' TERM
+trap 'trap - TERM; umount $LC_DIR; kill -s TERM -- -$$' TERM
 while true; do
-  mount.glusterfs $1:/gv0 /datavol
+  mount.glusterfs $LC_SRV:$GVOL $LC_DIR
   if [ $? -eq 0 ]
   then
     break
