5eb0c0a2f7c907061b7439dbd3761d0af98e7157
lukicsl <lukic@gmx.ch>
2019-01-11 10:38:14 +0100

    Update client.sh

--- client.sh
+++ client.sh
@@ -5,13 +5,14 @@ if (cat /etc/hostname-host) then
   LC_SRV=`cat /etc/hostname-host`
 fi
 echo $LC_SRV:$GVOL $LC_DIR
-sleep 10
+
 while true; do
   mount.glusterfs $LC_SRV:$GVOL $LC_DIR
   if [ $? -eq 0 ]
   then
     break
   fi
+  echo sleep 1s
   sleep 1
 done
 tail -f /dev/null & wait
